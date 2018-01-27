075b2288ad595003aa12e7eb213ef06e95b047e5
argentum <emin.azizov@gmail.com>
2018-01-27 23:12:16 +0400

    Update

--- source/reportDesignPanel.ts
+++ source/reportDesignPanel.ts
@@ -12,17 +12,18 @@ webix.protoUI({
             webix.DragControl.addDrop(panel, {
                 $drop(source: any, target: any, e: MouseEvent) {
                     const dnd = webix.DragControl.getContext();
-                    const off = getNodeOffset(panel);
+                    const off = webix.html.offset(panel);
                     console.log(dnd);
                     console.log(e);
                     console.log(off);
                     const el = document.createElement("div");
-                    el.style.position = "relative";
+                    el.className = "webix_reportitem selected";
+                    el.style.position = "absolute";
                     el.style.width = "150px";
                     el.style.height = "25px";
                     el.innerHTML = dnd.source[0];
-                    el.style.left = (e.pageX - off.left) + "px";
-                    el.style.top = (e.pageY - off.top) + "px";
+                    el.style.left = (e.pageX - off.x) + "px";
+                    el.style.top = (e.pageY - off.y) + "px";
                     el.style.border = "1px solid red";
                     panel.appendChild(el);
                 }
